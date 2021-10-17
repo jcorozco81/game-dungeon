d449a89b84bd63ddad4e00f69ba8a326f0004aec
jcorozco81 <jcorozco@gmail.com>
2021-10-16 22:24:14 -0400

    Debuging

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost/gamedungeon", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
+    useFindAndModify: false,
 })
 .then(console.log("Now connected to Mongodb"))
 .catch((err) => console.log(err));
