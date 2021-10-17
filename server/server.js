82ce22768f19d9eecb33c9cb66a60b5a367e2719
jcorozco81 <jcorozco@gmail.com>
2021-10-16 23:01:06 -0400

    Adding lines to server file

--- server/server.js
+++ server/server.js
@@ -33,6 +33,16 @@ mongoose.connect(process.env.MONGODB_URL, {
 // app.use(express.static(path.join(__dirname, 'test')));
 
 // Test End
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+
+
 
 
 
